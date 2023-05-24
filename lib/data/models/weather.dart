ef62bd749bb550ce60995933d1b9c9f67e9c9c38
Azizjon450 <qodirovazizjon.natura@gmail.com>
2023-05-24 15:42:10 +0500

    add weather model

---
+++ lib/data/models/weather.dart
@@ -0,0 +1,15 @@
+class Weather {
+  final String id;
+  final String main;
+  final String descrip;
+  final String icon;
+  final double temperature;
+
+  Weather({
+    required this.id,
+    required this.main,
+    required this.descrip,
+    required this.icon,
+    required this.temperature,
+  });
+}
